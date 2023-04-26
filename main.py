d7975f606568078f6eda261de8105baf1de90920
Your Name <you@example.com>
2023-04-26 14:04:33 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,370 @@
+# File created by: Domenico DiStefano
+'''
+Game Structure:
+Goals, Rules, Feedback, Freedom
+
+Sources:
+https://stackoverflow.com/questions/41348333/how-to-freeze-a-sprite-for-a-certain-amount-of-time-in-pygame
+https://stackoverflow.com/questions/30720665/countdown-timer-in-pygame
+
+Goal 1: create projectiles sprite ☑️
+Goal 2: create score & healthbar ☑️
+Goal 3: create start & end screen ☑️
+Goal 4: make it replayable ☑️
+
+Goal 5: add an upgrade system ☑️
+Goal 6: add music
+Goal 7: particles
+Goal 8: enemy wave system
+'''
+# import libs
+import pygame as pg
+import os
+# import settings 
+from settings import *
+from sprites import *
+import time
+# from pg.sprite import Sprite
+
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+
+# create game class in order to pass properties to the sprites file
+class Game:
+    
+    def __init__(self):
+        # initiate game window and game settings
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("ALIEN SWARM (⌐■_■)")
+        self.clock = pg.time.Clock()
+        self.running = True
+        self.startgame = False
+
+        # no damage mode for testing
+        self.godmode = False
+
+        # counter for time survived
+        self.alive = False
+        self.timeelapsed = 0
+        self.survivecounter = pg.USEREVENT+1
+        pg.time.set_timer(self.survivecounter, 1000)
+
+        # upgrade sreen settings and upgrade settings
+        self.upgradescreen = False
+        self.lifestealamount = 1
+        self.money = 0
+        self.teleport = False
+        self.multishot = 1
+
+        # timestop settings
+        self.timestopamount = 0
+        self.timestop = False
+        self.timestopcounter = False
+        self.counter = 3
+        self.timestoptimer = pg.USEREVENT+1
+        pg.time.set_timer(self.timestoptimer, 1000)
+
+        # firerate settings
+        self.firerate = 1
+        self.fireratescore = 1
+        self.togglefire = False
+        self.lastshot = time.time()
+
+    # method that adds sprites  
+    def new(self):
+        # starting a new game and adding sprites to groups
+        # separate groups for buttons and bullets so I can remove them individually
+        self.bullet_list = pg.sprite.Group()
+        self.button_list = pg.sprite.Group()
+        self.all_sprites = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player1 = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        self.player1.add(self.player)
+
+        # creating buttons
+        self.button1 = Button(self, 200, 25, BLACK, WIDTH/2, 357)
+        self.button2 = Button(self, 250, 25, BLACK, WIDTH/2, 407)
+        self.button3 = Button(self, 280, 25, BLACK, WIDTH/2, 457)
+        self.button4 = Button(self, 300, 25, BLACK, WIDTH/2, 507)
+        self.button5 = Button(self, 350, 25, BLACK, WIDTH/2, 557)
+
+        # makes range of mobs and adds them to all sprites group
+        for i in range(0,10):
+            self.mob1 = Mob(self, self.player, 20, 20,GREEN)
+            self.all_sprites.add(self.mob1)
+            self.enemies.add(self.mob1)
+         
+        self.run()
+
+    # method for running the game
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+            
+    # method for detecting events in game
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                # button for exiting game 
+                if event.key == pg.K_ESCAPE:
+                    self.startgame = True
+                    self.playing = False
+                    self.running = False
+
+                # creates projectile on player location and adds it to all sprites and bullet list toggle on/off
+                if event.key == pg.K_SPACE:
+                    if not self.togglefire:
+                        self.togglefire = True
+                    else:
+                        self.togglefire = False
+
+                # key that starts game and resets game settings and upgrades
+                if event.key == pg.K_p:
+                    self.startgame = True
+                    self.playing = False
+                    self.timestopamount = 0
+                    self.teleport = False
+                    self.mob1.enemyspeed = 0.05
+                    self.money = 0
+                    self.lifestealamount = 1
+                    self.firerate = 1
+                    self.fireratescore = 1
+                    self.multishot = 1
+                
+                # button for upgrade screen
+                if event.key == pg.K_u:
+                    if not self.upgradescreen:
+                        self.upgradescreen = True
+                    else:
+                        self.upgradescreen = False
+
+                # cheats for dev testing
+                if event.key == pg.K_0:
+                    self.money += 1000
+                if event.key == pg.K_9:
+                    if not self.godmode:
+                        self.godmode = True
+                    else:
+                        self.godmode = False
+
+                # teleport
+                if event.key == pg.K_e:
+                    mouse_coords = pg.mouse.get_pos()
+                    if self.teleport:
+                        self.player.pos = mouse_coords
+
+                # timestop
+                if event.key == pg.K_q:
+                    if self.timestopamount >= 1:
+                        self.timestopamount -= 1
+                        self.counter = 3
+                        self.timestopcounter = True
+            if event.type == pg.MOUSEBUTTONUP:
+                self.mousecollide()
+            if self.timestopcounter:
+                if event.type == self.timestoptimer:
+                    self.counter -= 1
+                    self.timestop = True
+                    if self.counter == 0:
+                        self.timestop = False
+                        self.timestopcounter = False
+            
+            # togglefire and multishot
+            if self.togglefire:
+                if time.time() - self.lastshot > 1/self.firerate:
+                    for x in range(0, self.multishot):
+                        bullet = Projectile(self, self.mob1, self.player)
+                        bullet.rect.x = self.player.pos.x 
+                        bullet.rect.y = self.player.pos.y -50
+                        self.all_sprites.add(bullet)
+                        self.bullet_list.add(bullet)
+                    self.lastshot = time.time()
+            
+            # time elapsed counter
+            if self.alive:
+                if event.type == self.survivecounter:
+                    self.timeelapsed += 1
+                    
+    def lifesteal(self):
+        self.player.hp += self.lifestealamount
+
+    # method that updates the game at 1/60th of a second
+    def update(self):
+        self.button_list.update()
+        if not self.upgradescreen and not self.timestop:
+            self.all_sprites.update()
+        elif not self.upgradescreen and self.timestop:
+            self.player1.update()
+        for bullet in self.bullet_list:
+        # See if it hit a block
+            self.enemy_hit_list = pg.sprite.spritecollide(bullet, self.enemies, True)
+            # For each enemy hit, remove the bullet and add to the score
+            for block in self.enemy_hit_list:
+                self.bullet_list.remove(bullet)
+                self.all_sprites.remove(bullet)
+                self.player.score += 1
+                self.money += 5
+                # adds a new enemy after one dies
+                self.mob1 = Mob(self, self.player, 20, 20,(0,255,0))
+                self.all_sprites.add(self.mob1)
+                self.enemies.add(self.mob1)
+
+                # makes mobs path better as you kill them and replenish hp on kill
+                self.mob1.enemyspeed += 0.01 
+                self.lifesteal()
+
+            # removes bullet if it exceeds a certain height or width
+            if bullet.pos.y > HEIGHT:
+                self.bullet_list.remove(bullet)
+                self.all_sprites.remove(bullet)
+            if bullet.pos.y < 0:
+                self.bullet_list.remove(bullet)
+                self.all_sprites.remove(bullet)
+            if bullet.pos.x > WIDTH:
+                self.bullet_list.remove(bullet)
+                self.all_sprites.remove(bullet)
+            if bullet.pos.x < 0:
+                self.bullet_list.remove(bullet)
+                self.all_sprites.remove(bullet)
+
+    # method for detecting mouse collisions with buttons
+    def mousecollide(self):
+        mouse_coords = pg.mouse.get_pos()
+        if self.button1.rect.collidepoint(mouse_coords):
+            if self.money >= 100 and not self.teleport:
+                self.teleport = True
+                self.money -= 100
+        elif self.button2.rect.collidepoint(mouse_coords):
+            if self.money >= 50:
+                self.timestopamount += 1
+                self.money -= 50
+        elif self.button3.rect.collidepoint(mouse_coords):
+            if self.money >= 25:
+                self.lifestealamount += 1
+                self.money -= 25
+        elif self.button4.rect.collidepoint(mouse_coords):
+            if self.money >= 10 and self.firerate > 0.2:
+                self.firerate += 1
+                self.fireratescore += 1
+                self.money -= 10
+        elif self.button5.rect.collidepoint(mouse_coords):
+            if self.money >= 5:
+                self.multishot += 1
+                self.money -= 5
+        
+
+    # method for displaying the game and displaying end screen when player hp = 0
+    def draw(self):
+        # start screen
+        if not self.startgame:
+            self.screen.fill(BLACK)
+            self.draw_text("ALIEN SWARM", 100, GREEN, WIDTH/2, 250)
+            self.draw_text("PRESS P TO PLAY", 40, WHITE, WIDTH/2, 330)
+            self.draw_text("WASD TO MOVE", 30, WHITE, WIDTH/2, 420)
+            self.draw_text("SPACE TO SHOOT", 30, WHITE, WIDTH/2, 450)
+            self.draw_text("KILL THE ALIENS TO REGAIN HP", 25, WHITE, WIDTH/2, 490)
+            self.draw_text("PRESS U TO UPGRADE", 25, WHITE, WIDTH/2, 510)
+        elif self.startgame and self.player.hp > 0 and self.upgradescreen:
+            if not self.teleport:
+                self.alive = False
+                self.togglefire = False
+                self.screen.fill(BLACK)
+                self.button_list.draw(self.screen)
+                self.draw_text("UPGRADES", 100, WHITE, WIDTH/2, 250)
+                self.draw_text("MONEY: $" + str(self.money), 30, WHITE, WIDTH/2, 600)
+                self.draw_text("COST $100 - TELEPORT ABILITY", 30, WHITE, WIDTH/2, 350)
+                self.draw_text("COST $50 - TIMESTOP ABILITY: " + str(self.timestopamount), 30, WHITE, WIDTH/2, 400)
+                self.draw_text("COST $25 - LIFESTEAL AMOUNT: " + str(self.lifestealamount), 30, WHITE, WIDTH/2, 450)
+                if self.fireratescore < 10:
+                    self.draw_text("COST $10 - FIRERATE: " + str(self.fireratescore) + "/SEC", 30, WHITE, WIDTH/2, 500)
+                else:
+                    self.draw_text("FIRERATE MAXED", 30, WHITE, WIDTH/2, 500)
+                self.draw_text("COST $5 - MULTISHOT AMOUNT: " + str(self.multishot), 30, WHITE, WIDTH/2, 550)
+                # adds clickable buttons with button class
+                self.button_list.add(self.button1)
+                self.button_list.add(self.button2)
+                self.button_list.add(self.button3)
+                self.button_list.add(self.button4)
+                self.button_list.add(self.button5)
+                self.draw_text("PRESS Q TO TIMESTOP", 30, WHITE, WIDTH/2, 650)
+            elif self.teleport:
+                self.alive = False
+                self.togglefire = False
+                self.screen.fill(BLACK)
+                self.button_list.draw(self.screen)
+                self.draw_text("UPGRADES", 100, WHITE, WIDTH/2, 250)
+                self.draw_text("MONEY: $" + str(self.money), 30, WHITE, WIDTH/2, 600)
+                self.draw_text("TELEPORT ABILITY GAINED", 30, WHITE, WIDTH/2, 350)
+                self.draw_text("COST $50 - TIMESTOP ABILITY: " + str(self.timestopamount), 30, WHITE, WIDTH/2, 400)
+                self.draw_text("COST $25 - LIFESTEAL AMOUNT: " + str(self.lifestealamount), 30, WHITE, WIDTH/2, 450)
+                if self.fireratescore < 10:
+                    self.draw_text("COST $10 - FIRERATE: " + str(self.fireratescore) + "/SEC", 30, WHITE, WIDTH/2, 500)
+                else:
+                    self.draw_text("FIRERATE MAXED", 30, WHITE, WIDTH/2, 500)
+                self.draw_text("COST $5 - MULTISHOT AMOUNT: " + str(self.multishot), 30, WHITE, WIDTH/2, 550)
+                # adds clickable buttons with button class
+                self.button_list.add(self.button1)
+                self.button_list.add(self.button2)
+                self.button_list.add(self.button3)
+                self.button_list.add(self.button4)
+                self.button_list.add(self.button5)
+                self.draw_text("PRESS E TO TELEPORT", 30, WHITE, WIDTH/2, 670)
+                self.draw_text("PRESS Q TO TIMESTOP", 30, WHITE, WIDTH/2, 650)
+        
+            
+        else:
+            # main game screen
+            if self.player.hp > 0:
+                self.alive = True
+                self.screen.fill(BLACK)
+                self.all_sprites.draw(self.screen)
+                self.draw_text("HP: " + str(self.player.hp), 30,WHITE, 1100, HEIGHT/32)
+                self.draw_text("ELIMINATIONS: " + str(self.player.score), 30,WHITE, 120, HEIGHT/32)
+                self.draw_text("TIME SURVIVED: " + str(self.timeelapsed) + " SECONDS", 30, WHITE, WIDTH/2, HEIGHT/32)
+                self.draw_text("MONEY: $" + str(self.money), 30, WHITE, 83, HEIGHT/32 +30)
+                self.draw_text("TIMESTOPS: " + str(self.timestopamount), 30, WHITE, 106, HEIGHT/32 +60)
+            # end screen
+            elif self.player.hp <= 0:
+                self.alive = False
+                self.togglefire = False
+                self.screen.fill(BLACK)
+                self.draw_text("YOU DIED", 100, RED, WIDTH/2, 250)
+                self.draw_text("PLAY AGAIN? (P)", 30, WHITE, WIDTH/2, 450)
+                self.draw_text("ELIMINATIONS: " + str(self.player.score), 30, WHITE, WIDTH/2, 350)
+                self.draw_text("TIME SURVIVED: " + str(self.timeelapsed) + " SECONDS", 30, WHITE, WIDTH/2, 400)
+                # removes all sprites to stop any updates while not visible
+                self.all_sprites.empty()
+
+    
+
+        pg.display.flip()
+    
+    # method for drawing text
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('Monaco')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
+    
+# instantiate the game class
+g = Game()
+# starts the game loop
+while g.running:
+    g.new()
+pg.quit()
---
+++ sprites.py
@@ -0,0 +1,194 @@
+import pygame as pg
+from pygame.sprite import Sprite
+from settings import *
+from random import randint
+import math
+import datetime
+
+
+vec = pg.math.Vector2
+
+# player class
+class Player(Sprite):
+    def __init__(self, game):
+        Sprite.__init__(self)
+        self.game = game
+        self.image = pg.Surface((50,50))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.hp = HEALTH
+        self.score = SCORE
+
+    # method that takes user input and applies acceleration to the sprite
+    def input(self):
+        keystate = pg.key.get_pressed()
+
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+        if keystate[pg.K_w]:
+            self.acc.y = -PLAYER_ACC
+        if keystate[pg.K_s]:
+            self.acc.y = PLAYER_ACC
+        
+    # this is a method that will keep the sprite on screen
+    def inbounds(self):
+        width = 50
+        height = 50
+        if self.rect.x > WIDTH - width:
+            self.pos.x = WIDTH - width/2
+            self.vel.x = 0
+        if self.rect.x < 0:
+            self.pos.x = width/2
+            self.vel.x = 0
+        if self.rect.y > HEIGHT - height:
+            self.pos.y = HEIGHT - height/2
+            self.vel.y = 0
+        if self.rect.y < 0:
+            self.pos.y = height/2
+            self.vel.y = 0
+    
+    # method that detects mob collision and then subtracts from hp pool and slows player when colliding
+    def mob_collide(self):
+        hits = pg.sprite.spritecollide(self, self.game.enemies, False)
+        if hits and not self.game.godmode:
+            self.vel.x *= 0
+            self.vel.y *= 0
+            self.hp -= 1       
+    
+    # method that updates values every 1/60th of a second
+    def update(self):
+        self.inbounds()
+        self.mob_collide()
+        self.acc = self.vel * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
+
+# class for mobs
+class Mob(Sprite):
+    # parameters for mob class
+    def __init__(self, game, player, width, height, color):
+        Sprite.__init__(self)
+        # allows us to use the player and game's parameters inside for the mob (used in the inbounds method)
+        self.player = player
+        self.game = game
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        # randomizes starting position and velocity
+        self.pos = vec(randint(0,WIDTH),randint(0,50))
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.cofric = 0.1
+        self.enemyspeed = .05
+        
+    # keeps mob inbounds adjusting it to its own width that is determined when an instance of the class is created
+    def inbounds(self):
+        if self.rect.x > WIDTH - self.width:
+            self.pos.x = WIDTH - self.width/2
+            self.vel.x = 0
+        if self.rect.x < 0:
+            self.pos.x = self.width/2
+            self.vel.x = 0
+        if self.rect.y > HEIGHT - self.height:
+            self.pos.y = HEIGHT - self.height/2
+            self.vel.y = 0
+        if self.rect.y < 0:
+            self.pos.y = self.height/2
+            self.vel.y = 0
+
+    # method for rudimentary pathing used to track and follow player
+    def pathing(self):
+        if self.player.pos.x > self.pos.x:
+            self.vel.x += self.enemyspeed
+        if self.player.pos.x < self.pos.x:
+            self.vel.x -= self.enemyspeed
+        if self.player.pos.y < self.pos.y:
+            self.vel.y -= self.enemyspeed
+        if self.player.pos.y > self.pos.y:
+            self.vel.y += self.enemyspeed
+
+    # method for determining player collision and then stops the mob from continuing to move
+    def player_collide(self):
+        hits = pg.sprite.spritecollide(self, self.game.player1, False)
+        if hits:
+            self.vel.x *= 0
+            self.vel.y *= 0
+            
+    # updates mob class when called
+    def update(self):
+        self.inbounds()
+        self.pathing()
+        self.player_collide()
+        self.pos += self.vel
+        self.rect.center = self.pos
+
+# class for projectiles
+class Projectile(Sprite):
+    def __init__(self, game, enemies, player):
+        # allows me to call methods from the parent class and returns a temporary object
+        # parent class is the class that the class is being inherited from
+        super().__init__()
+
+        # allows the projectile class to make use of the parameters of other classes
+        self.game = game
+        self.enemies = enemies
+        self.player = player
+        self.projectilespeed = 1
+        self.pos = vec(50,50)
+        self.pos.x = self.player.pos.x
+        self.pos.y = self.player.pos.y
+        self.vel = vec(randint(1,5),randint(1,5))
+        self.acc = vec(1,1)
+        self.image = pg.Surface([10, 10])
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+
+    # method for rudimentary tracking by changing velocities depending on enemy position
+    def tracking(self):
+        if self.enemies.pos.x > self.pos.x:
+            self.vel.x -= self.projectilespeed
+        if self.enemies.pos.x < self.pos.x:
+            self.vel.x += self.projectilespeed
+        if self.enemies.pos.y < self.pos.y:
+            self.vel.y += self.projectilespeed
+        if self.enemies.pos.y > self.pos.y:
+            self.vel.y -= self.projectilespeed
+
+    # updates sprite when called
+    def update(self):
+        self.tracking()
+        self.pos += self.vel * MOB_FRICTION
+        self.rect.center = self.pos
+
+class Button(Sprite):
+    # parameters for mob class
+    def __init__(self, game, width, height, color,x,y):
+        Sprite.__init__(self)
+        # allows us to use the player and game's parameters inside for the mob (used in the inbounds method)
+        self.game = game
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect()
+        # randomizes starting position and velocity
+        self.pos = vec(x,y)
+        
+
+    
+    def update(self):
+        # self.mousecollide()
+        self.rect.center = self.pos
